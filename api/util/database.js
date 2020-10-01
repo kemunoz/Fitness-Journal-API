9d41f54e00940f73e27e7f0a8f3ea30c8ff02788
Kevin Munoz <munozk341@gmail.com>
2020-10-01 15:40:17 -0700

    trying eithout port

--- api/util/database.js
+++ api/util/database.js
@@ -4,8 +4,7 @@ const fixieURL = process.env.FIXIE_SOCKS_HOST;
 const fixieValues = fixieURL.split(new RegExp('[/(:\\/@)/]+'));
 
 const mysqlServer = {
-    host: '34.94.232.187',
-    port: 3306
+    host: '34.94.232.187'
 };
 
 const fixieConnection = new SocksConnection(mysqlServer, {
