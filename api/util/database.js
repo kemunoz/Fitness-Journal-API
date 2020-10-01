00a31e9092f76a01bbeaddd3d37cb970f1505170
Kevin Munoz <munozk341@gmail.com>
2020-10-01 15:31:16 -0700

    added fixie socks

--- api/util/database.js
+++ api/util/database.js
@@ -1,11 +1,25 @@
 const mysql = require('mysql');
+const SocksConnection = require('socksjs');
+const fixieURL = process.env.FIXIE_SOCKS_HOST;
+const fixieValues = fixieUrl.split(new RegExp('[/(:\\/@)/]+'));
 
-
-const connection = mysql.createPool({
+const mysqlServer = {
     host: '34.94.232.187',
-    user: 'root',
-    password: `${process.env.CLOUD_SQL_PASSWORD}`,
-    database: `${process.env.DATABASE_NAME}`
+    port: 3306
+};
+
+const fixiConnection = new SocksConnection(mysqlServer, {
+    user: fixieValues[0],
+    pass: fixieValues[1],
+    host: fixieValues[2],
+    port: fixieValues[3],
 });
 
-module.exports = connection;
+const mysqlConnPool = mysql.createPool({
+    user: 'root',
+    password: `${process.env.CLOUD_SQL_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
+    stream: fixieConnection
+});
+
+module.exports = mysqlConnPool;
