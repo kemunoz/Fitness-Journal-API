0afb964f08ebb35f120ecc5ce82411d0606f26ce
Kevin Munoz <munozk341@gmail.com>
2020-10-03 09:41:12 -0700

    removed fixie

--- api/util/database.js
+++ api/util/database.js
@@ -7,18 +7,10 @@ const mysqlServer = {
     host: '34.94.232.187'
 };
 
-const fixieConnection = new SocksConnection(mysqlServer, {
-    user: fixieValues[0],
-    pass: fixieValues[1],
-    host: fixieValues[2],
-    port: fixieValues[3],
-});
-
 const mysqlConnPool = mysql.createPool({
     user: 'root',
     password: `${process.env.CLOUD_SQL_PASSWORD}`,
     database: `${process.env.DATABASE_NAME}`,
-    stream: fixieConnection
 });
 
 module.exports = mysqlConnPool;
