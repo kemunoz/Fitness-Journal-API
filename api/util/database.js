9be8a9aaa57524cbc447ba6ab1b77a78e666c355
Kevin Munoz <munozk341@gmail.com>
2020-10-07 15:59:41 -0700

    added jawsDB

--- api/util/database.js
+++ api/util/database.js
@@ -1,16 +1,5 @@
 const mysql = require('mysql');
-const SocksConnection = require('socksjs');
-const fixieURL = process.env.FIXIE_SOCKS_HOST;
-const fixieValues = fixieURL.split(new RegExp('[/(:\\/@)/]+'));
 
-const mysqlServer = {
-    host: '34.94.232.187'
-};
-
-const mysqlConnPool = mysql.createPool({
-    user: 'root',
-    password: `${process.env.CLOUD_SQL_PASSWORD}`,
-    database: `${process.env.DATABASE_NAME}`,
-});
+const mysqlConnPool = mysql.createPool(process.env.JAWSDB_URL);
 
 module.exports = mysqlConnPool;
